8a9b61366764f05b697bb7502e88e4a31884e295
DonKiwi-cyber <minecraft200294@gmail.com>
2025-07-18 18:57:14 -0600

    drizzle parse

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from "drizzle-kit"
+import { Config } from "drizzle-kit"
 
 //Define la URL de la base de datos desde las variables de entorno
 const databaseUrl = process.env.DATABASE_URL
@@ -9,13 +9,8 @@ if (!databaseUrl){
 }
 
 //Exporta la configuración de Drizzle con la ayuda de "defineConfig"
-export default defineConfig({
+export default {
     schema: "./drizzle/schema.ts", //La definición del esquema de la BD
     out: "./drizzle/migrations", //El directorio donde se mandaán los archivos de migración
-    dialect: "postgresql", //El lenguaje que manejará la BD
-    strict: true, //Activa las validaciones estrictas y de tipo seguro
-    verbose: true, //Permite el logging verbal para obtener más información durante las acciones del Cliente
-    dbCredentials: {
-        url: databaseUrl
-    }
-})
+} satisfies Config
+
